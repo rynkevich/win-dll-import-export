553bd238d69dbe33559cefde4488ad26f7a786a0
NRGb3nder <energybender120@gmail.com>
2018-09-29 02:13:28 +0300

    Refactor static import demo

--- StaticImportDemo/main.cpp
+++ StaticImportDemo/main.cpp
@@ -3,34 +3,45 @@
 #include "main.h"
 
 #define MIN_VALID_ARGC 4
-#define USAGE_MSG "Usage: <process_name> <string_to_replace> <new_string> [<dump_before_replace_filename>] [<dump_after_replace_filename>]"
+#define TEXTMSG_USAGE "Usage: <process_name> <string_to_replace> <new_string> "\
+                      "[<dump_before_replace_filename>] [<dump_after_replace_filename>]"
+#define TEXTMSG_PROCESS_NOT_FOUND "Process not found"
+#define TEXTMSG_DONE "Done."
+
+INT DumpMemAndReport(PCSTR pszProcessName, PCSTR pszDumpFilePath);
 
 INT main(INT argc, PCHAR argv[])
 {
     if (argc < MIN_VALID_ARGC) {
-        puts(USAGE_MSG);
+        puts(TEXTMSG_USAGE);
         return 1;
     }
 
-    CHAR szDumpBeforeReplaceFullPath[MAX_PATH];
-    CHAR szDumpAfterReplaceFullPath[MAX_PATH];
-    GetFullPathNameA(argv[4], MAX_PATH, szDumpBeforeReplaceFullPath, NULL);
-    GetFullPathNameA(argv[5], MAX_PATH, szDumpAfterReplaceFullPath, NULL);
-
-    if (argc > MIN_VALID_ARGC + 1) {
-        DumpMem(argv[1], argv[4]);
+    if (argc > MIN_VALID_ARGC + 1 && DumpMemAndReport(argv[1], argv[4])) {
+        return 1;
+    }
+    if (ReplaceString(argv[1], argv[2], argv[3])) {
+        puts(TEXTMSG_PROCESS_NOT_FOUND);
+        getchar();
+        return 1;
+    } 
+    if (argc > MIN_VALID_ARGC + 2 && DumpMemAndReport(argv[1], argv[5])) {
+        return 1;
     }
 
-    INT iErrCode;
-    if (iErrCode = ReplaceString(argv[1], argv[2], argv[3])) {
-        puts("Process not found.");
-    } else {
-        puts("Done.");
-        if (argc > MIN_VALID_ARGC + 2) {
-            DumpMem(argv[1], argv[5]);
-        }
-    }
-
+    puts(TEXTMSG_DONE);
     getchar();
+    return 0;
+}
+
+INT DumpMemAndReport(PCSTR pszProcessName, PCSTR pszDumpFilePath) 
+{
+    CHAR szDumpFileFullPath[MAX_PATH];
+    GetFullPathNameA(pszDumpFilePath, MAX_PATH, szDumpFileFullPath, NULL);
+    if (DumpMem(pszProcessName, szDumpFileFullPath)) {
+        puts(TEXTMSG_PROCESS_NOT_FOUND);
+        getchar();
+        return 1;
+    }
     return 0;
 }
